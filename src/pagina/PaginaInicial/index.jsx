56be719759e45111b26b91a192c3ac1c7be68de3
Lucas Freitas <lucasfsouzaa1@gmail.com>
2023-06-21 21:13:58 -0300

    adicionando banner

--- src/pagina/PaginaInicial/index.jsx
+++ src/pagina/PaginaInicial/index.jsx
@@ -1,11 +1,21 @@
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
+import banner from "./banner.png"
+import styles from "./paginaInicial.module.scss"
 
 export default function PaginaInicial() {
     return (
         <>
             <Cabecalho />
+            <main>
+                <section className={styles.principal}>
             <Menu />
+            <div className={styles.principal__imagem}>
+                <h1>A galeria mais completa do espaço</h1>
+                <img src={banner} alt="A imagem da terra vista do espaço" />
+            </div>
+                </section>
+            </main>
         </>
     )
 }
