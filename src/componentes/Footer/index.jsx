2c2b0a7696237f4b86d211c17972b2d52043e914
Lucas Freitas <lucasfsouzaa1@gmail.com>
2023-06-22 22:56:43 -0300

    alteracao footer

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import facebook from './facebook.svg'
-import instagram from './instagram.svg'
-import twitter from './twitter.svg'
-import styles from './Footer.module.scss'
+import React from "react";
+import facebook from "./facebook.svg";
+import twitter from "./twitter.svg";
+import instagram from "./instagram.svg";
+import styles from "./Footer.module.scss";
 
-export default function Footer() {
-    return (
-        <footer className={styles.rodape}>
-            <div className={styles.icones}>
-                <img src={facebook} alt="logo do facebook" />
-                <img src={twitter} alt="logo do twitter" />
-                <img src={instagram} alt="logo do instagram" />
-            </div>
-            <div>
-                <h4 className={styles.texto}>Desenvolvivo por Alura.</h4>
-            </div>
-        </footer>
-    )
+export default function Rodape() {
+  return (
+    <footer className={styles.rodape}>
+      <div className={styles.rodape__icones}>
+        <a
+          href="https://www.facebook.com/AluraCursosOnline/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={facebook} alt="ícone do facebook" />
+        </a>
+        <a
+          href="https://twitter.com/aluraonline?lang=en"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={twitter} alt="ícone do twitter" />
+        </a>
+        <a
+          href="https://www.instagram.com/aluraonline/?hl=en"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={instagram} alt="ícone do instagram" />
+        </a>
+      </div>
+      <p>Desenvolvido por Alura</p>
+    </footer>
+  );
 }
