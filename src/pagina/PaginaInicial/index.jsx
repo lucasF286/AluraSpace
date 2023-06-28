2c23b01386dfd481049e87f6057aab2fca9b7d8b
Lucas Freitas <lucasfsouzaa1@gmail.com>
2023-06-27 22:29:31 -0300

    inclusao secao de populares

--- src/pagina/PaginaInicial/index.jsx
+++ src/pagina/PaginaInicial/index.jsx
@@ -2,6 +2,7 @@ import Cabecalho from "../../componentes/Cabecalho";
 import Footer from "../../componentes/Footer";
 import Galeria from "../../componentes/Galeria";
 import Menu from "../../componentes/Menu";
+import Populares from "../../componentes/Populares";
 import banner from "./banner.png"
 import styles from "./paginaInicial.module.scss"
 
@@ -11,16 +12,17 @@ export default function PaginaInicial() {
             <Cabecalho />
             <main>
                 <section className={styles.principal}>
-            <Menu />
-            <div className={styles.principal__imagem}>
-                <h1>A galeria mais completa do espaço</h1>
-                <img src={banner} alt="A imagem da terra vista do espaço" />
-            </div>
+                    <Menu />
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt="A imagem da terra vista do espaço" />
+                    </div>
                 </section>
+                <div className={styles.galeria}>
+                    <Galeria />
+                    <Populares />
+                </div>
             </main>
-            <div>
-                <Galeria />
-            </div>
             <Footer />
         </>
     )
